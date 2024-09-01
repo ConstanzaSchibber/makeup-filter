0092bdbedad8fc0decd916f42c3321157c0b35f8
Constanza Schibber <constanza.fs@gmail.com>
2024-09-01 16:55:59 +0000

    import resolved

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-#from st_clickable_images import clickable_images
+from st_clickable_images import clickable_images
 import pandas as pd
 from streamlit_dynamic_filters import DynamicFilters
 from PIL import Image
